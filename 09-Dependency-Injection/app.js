355648e5f913f235fb0fd35e66d59c9bbabddf1e
cu <harrierraum@hotmail.com>
2021-03-16 00:16:11 +0100

    add injector

--- 09-Dependency-Injection/app.js
+++ 09-Dependency-Injection/app.js
@@ -4,7 +4,15 @@
   angular.module('DIApp', [])
   .controller('DIController', DIController);
 
-  function DIController ($scope) {
+  function DIController ($scope,
+                         $filter,
+                         $injector) {
     $scope.name = "Yaakov";
+
+    $scope.upper = function () {
+      const upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    };
+    console.log($injector.annotate(DIController));
   }
 })();
